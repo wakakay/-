8d57671b4deb2c0550caa279b9b0498f10f7cddf
zebin.lin <wakakay>
2018-12-24 16:44:07 +0800

    MVP10.4.1

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+import common from './common' // 公共接口
 import * as auth from './auth'
 import * as course from './course'
 import * as card from './card'
@@ -10,9 +11,13 @@ import * as contact from './contact'
 import * as score from './score'
 import * as practice from './practice'
 import * as report from './report'
-import registered from './registered'
+import registered from './registered' // 新增的接口，待整合
+
+import _ from 'underscore'
+const fetch = _.extend(common, registered)
 
 export {
+    fetch,
     auth,
     course,
     card,
--- src/api/registered.js
+++ src/api/registered.js
@@ -8,70 +8,7 @@
  * 2018/9/19      1.0     First version
  *
  */
-import wepy from 'wepy'
-import config from './config'
-import _ from 'underscore'
-import { UnAuthenticationError } from '../errors'
-
-/**
- * 参数进行分割
- * @param data
- * @returns {{string: {字符串的键值对}, array: {数组对象的键值对}}}
- */
-const paramsData = (data) => {
-    let result = {
-        string: {},
-        array: {},
-        object: {}
-    }
-    let params = []
-    _.mapObject(data, function (val, key) {
-        if (_.isArray(val)) {
-            result.array[key] = val
-        } else if (_.isObject(val)) {
-            result.object = val
-        } else {
-            params.push(`${key}=${val}`)
-        }
-    })
-    result.string = '?' + params.join('&')
-    return result
-}
-
-const fetch = (actionObj) => {
-    // 没有token的直接抛错
-    if (!actionObj.isVisitor && ('defaultToken' === actionObj.params.token || !actionObj.params.token)) {
-        throw new UnAuthenticationError()
-    }
-
-    let params = paramsData(actionObj.params)
-    let requestData = {
-        url: `${config.baseUrl + actionObj.url + params.string}`,
-        method: actionObj.method,
-        data: !_.isEmpty(params.object) ? params.object : params.array
-    }
-
-    return wepy.request(requestData).then((respone) => {
-        let data = respone.data
-        if (!_.isUndefined(data.success)) {
-            if (actionObj.isUnFilter) {
-                return data
-            } else {
-                if (1 === data.success) {
-                    return data.data
-                } else {
-                    wx.showToast({title: data.message})
-                }
-            }
-        } else {
-            if (200 !== data.status) {
-                throw new Error(data.message)
-            } else {
-                return data.data
-            }
-        }
-    })
-}
+import {fetch} from './fetch-utils'
 
 export default {
     // 获取职能跟对应的兴趣
@@ -231,6 +168,5 @@ export default {
     //获取全部未学课程
     todayToLearnCourses(params) {
         return fetch({method: 'post', url: 'todayTask/toLearnCourses', params: params, isVisitor: true})
-    },
-
+    }
 }
